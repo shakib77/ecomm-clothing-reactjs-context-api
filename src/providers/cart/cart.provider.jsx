fbe68df2a3dca6e042ee8b6406ff96628b1ce662
shakib77 <shakibshaon77@gmail.com>
2020-12-01 21:33:15 +0600

    created provider context component, injected in other file to cart dropdown function

---
+++ src/providers/cart/cart.provider.jsx
@@ -0,0 +1,39 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+import { addItemToCart, removeItemFromCart } from './cart.utils';
+
+export const CartContext = createContext({
+    hidden: true,
+    toggleHidden: () => {},
+    cartItems: [],
+    addItem: () => {},
+    removeItem: () => {},
+    clearItemFromCart: () => {},
+    cartItemsCount: 0
+});
+
+const CartProvider = ({ childern }) => {
+    const [hidden, setHidden] = useState(true);
+    const [cartItems, setCartItems] = useState([]);
+    const [cartItemsCount, setCartItemsCount] = useState(0);
+
+    const addItem = item => setCartItems(addItemToCart(cartItems, item));
+    const toggleHidden = () => setHidden(!hidden);
+
+    return (
+        <CartContext.Provider
+            value={{
+                hidden,
+                toggleHidden,
+                cartItems,
+                addItem,
+                cartItemsCount
+            }}
+        >
+            {childern}
+        </CartContext.Provider>
+    );
+};
+
+
+export default CartProvider;
